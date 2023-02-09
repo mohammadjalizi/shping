ad1bd9923a3a449d1bc154d652cd18c576cd2ecf
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-09 22:13:47 +0330

    added project

--- src/compennet/Shop.js
+++ src/compennet/Shop.js
@@ -1,38 +1,9 @@
-import React, { Component } from 'react'
-import img1 from "../compennet/images/Album 1.png"
-import img2 from "../compennet/images/Album 2.png"
-import img3 from "../compennet/images/Album 3.png"
-import Proudoct from './Proudoct'
-export default class Shop extends Component {
-    constructor (props){
- super(props)
+import React from 'react'
 
- this.state={
-    proudoctsid:[
-{ id:1  , title:'album1',price:5,img: img1    },
-{ id:2  , title:'album2',price:5,img: img2   },
-{ id:3 , title:'album3',price:5,img: img3    },
-
-
-
-
-    ]
-   
-
-
- }
-
-    }
-  render() {
-    return (
-      <div>
-{this.state.proudoctsid.map(proudoct=>(
-<Proudoct   {...proudoct2}   />
-
-))}
-
-
-      </div>
-    )
-  }
+function Shop() {
+  return (
+    <div>Shop</div>
+  )
 }
+
+export default Shop
