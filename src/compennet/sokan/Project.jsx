94fd3b683bb2f72927b9dea084839c11db9a505e
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-13 15:06:20 +0330

    Added projec

--- src/compennet/sokan/Project.jsx
+++ src/compennet/sokan/Project.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import Array from '../sokan/proarry.json'
 const Project = () => {
   return (
-    <div>Project</div>
+    <div className=' h-[100vh] '>Project
+    
+   {Array.map((item=>(
+  <h1>{item.name}</h1>
+
+   )))}
+    
+    </div>
   )
 }
 
