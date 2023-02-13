b5ba3131778aa5026e4847c3a32f5e9e085d475a
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-13 14:32:18 +0330

    Added project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Shop from './compennet/Shop'
+import Project from './compennet/sokan/Project'
 function App() {
   return (
     <div>
 
 <Shop/>
+<Project/>
     </div>
   )
 }
