7e0afdf0fb83882badf261cb6c522a333176670c
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-14 14:23:48 +0330

    added project

--- src/compennet/sokan/All.jsx
+++ src/compennet/sokan/All.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Navbar from './Navbar'
+import Navbar from './Navbar/Navbar'
 import Project from './proudocts/Project'
-import Footer from './Footer'
+import Footer from './footer/Footer'
 function All() {
   return (
   <>
