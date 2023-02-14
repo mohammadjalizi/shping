7b2bb414662df45f99fb2c13279dd08bebc9ba00
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-14 15:24:10 +0330

    Added project

--- src/compennet/sokan/All.jsx
+++ src/compennet/sokan/All.jsx
@@ -3,13 +3,12 @@ import Navbar from './Navbar/Navbar'
 import ALLProudocts from './proudocts/ALLProudocts'
 import Footer from './footer/Footer'
 
-import ProudoctsD from './proudocts/Data/Proudocts'
 function All() {
   return (
   <>
   <Navbar/>
 
-<ProudoctsD  ProudoctData={ALLProudocts}/>
+
   <Footer/>
   
   </>
