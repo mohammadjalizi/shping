1c12eec4a7e03f5e1a2cd865ba2a3f767ca3fb59
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-15 15:09:44 +0330

    Added project

---
+++ src/compennet/Cimpennet.jsx
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export default class Cimpennet extends Component {
+
+
+  render() {
+    return (
+      <div>Cimpennet</div>
+    )
+  }
+}
