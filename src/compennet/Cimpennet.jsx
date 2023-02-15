cc0bbd9503a6cb9ad483d0cae48505cecc479ea0
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-15 22:41:04 +0330

    Added project

--- src/compennet/Cimpennet.jsx
+++ src/compennet/Cimpennet.jsx
@@ -2,14 +2,33 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function Cimpennet() {
-    const[deta,setData]=useState([])
+    // const[deta,setData]=useState([])
 
 
 useEffect(() => {
-axios.get("https://jsonplaceholder.typicode.com/posts")
-.then(Response=>console.log(Response))
 
-}, [])
+  // Get
+// axios.get("https://jsonplaceholder.typicode.com/posts")
+// .then(Response=>console.log(Response))
+
+//Post
+
+console.log("request has been sent!");
+setLoading(true);
+axios.get("https://reqres.in/api/users?delay=3")
+.then(response => {
+  // Successfull response
+  setLoading(false);
+  console.log("response received");
+});
+
+
+
+
+
+
+}, []);
+
 
   return (
     <div>
