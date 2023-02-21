77a5e76976946d7d324f3e6d8cbebfdbcf0b41bc
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-21 16:57:16 +0330

    Added project

---
+++ src/ProdactsNew/shoping.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const MUlist=[
+
+{id:1,name:"mohammad"},
+{id:2,name:"ali"},
+{id:3,name:"hassan"},
+{id:4,name:"ahwazi"},
+
+]
+
+
+function shoping() {
+  return (
+    <div>shoping</div>
+  )
+}
+
+export default shoping
