4aeb9afc18dc747bdefa20f7355c2784ee8e51e9
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-21 17:07:32 +0330

    Added project

---
+++ src/Data/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Card(props) {
+  return (
+    <div>
+        
+<h1>  {props.title} </h1>
+
+    </div>
+  )
+}
+
+export default Card
--- src/Data/Data.jsx
+++ src/Data/Data.jsx
@@ -7,3 +7,7 @@ const Data=[
 
 
 ]
+
+
+
+export default Data;
