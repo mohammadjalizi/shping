534c73514df8317283594849a01b1f39644db94d
mohammadjalizi <mjalizi1376@gmail.com>
2023-02-23 15:31:11 +0330

    Added project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Card from './Data/Card'
-
+import Data from './Data/Data'
 function App() {
+const datamap=Data.map((data)=>{
 
+  <Card  key={data.id} title={data.title}  />
+})
   return (
 <>
 
