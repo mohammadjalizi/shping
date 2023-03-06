4ac2e1c6dfaaf7f245f2e7e36db231025aab1df7
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-06 18:59:38 +0330

    added project

---
+++ src/Map.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Map() {
+    const propslist=[
+{id:1,name:'mohammad',price:33},
+{id:2,name:'ali',price:44},
+{id:3,name:'jalizi',price:77},
+
+    ]
+  return (
+    <div>Map</div>
+  )
+}
+
+export default Map
